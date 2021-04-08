171aef80ba4fe2b9b0257d381cbb5c2d37ff565b
Yuma Kinoshita <ykinoshita@tmu.ac.jp>
2021-04-08 17:09:13 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import argparse
+from pathlib import Path
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+
+if __name__ == "__main__":
+    t = np.linspace(0, 1, num=1000)
+    f = 1
+    y = np.sin(2*np.pi*f*t)
+    plt.figure()
+    plt.plot(t, y, color="black")
+    plt.axis("off")
+    plt.savefig("output.pdf")
